cf646e8b69742b11886f6e813793cfb0257b143b
Kamynsky <mkamynsky@gmail.com>
2025-01-03 15:28:38 +0100

    addied word count to main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def openbook(book):
     with open(book) as f:
+        book_content=()
         book_content=f.read()
-        return book_content
+        content_list=[]
+        content_list=book_content.split()
+        word_count=len(content_list)
+        
+        return book_content, word_count
     
 print(openbook("books/frankenstein.txt"))
