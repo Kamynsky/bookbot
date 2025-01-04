c733b366065f264c6e9bdf21ebf54f260b3bd92f
Kamynsky <mkamynsky@gmail.com>
2025-01-04 20:21:23 +0100

    added Count Characters function to main.py

--- main.py
+++ main.py
@@ -5,14 +5,26 @@ def openbook(book):
         return book_content
 def wordcounts(book_content):
     word_count=len(book_content.split())
-    print("word count :",word_count)
-def words
+    print(word_count,"words found in the document")
+def charcount(book_content):
+    lower_string=book_content.lower()
+    char_dict={}
+    for k in lower_string:
+        if k.islower():
+            if k not in char_dict:
+                char_dict[k]=1
+            else:
+                char_dict[k]+=1
+        else:
+            continue
+    sorted_char_dict=dict(sorted(char_dict.items(),key=lambda item: item[1],reverse=True))
+    for item in sorted_char_dict:
+        print(f"The '{item}' character was found {sorted_char_dict[item]} times")
+            
     
 
     
 book_content=openbook("books/frankenstein.txt")
 wordcounts(book_content)
+charcount(book_content)
 
-# Add a new function to your script that takes the text from the book as a string, and returns the number of times each character appears in the string. Convert any character to lowercase, we don't want duplicates.
-# use dict to count number of words
-# use def for each function
